29aef8a6de661e5c59236dc6b6df7a848335d51f
Franco Santarelli <fhsantarelli@gmail.com>
2020-03-29 13:19:29 -0300

    Ejercicio 9

---
+++ Practicas/practicas.ORT/src/practicas/ORT/Ejercicio9_TP1.java
@@ -0,0 +1,39 @@
+package practicas.ORT;
+
+/*Dados 2 valores numéricos num1 y num2, ingresados por teclado, asignar en la
+variable booleana sonIguales el valor correspondiente a partir de los valores de la
+comparación entre ambos (verdadero si son iguales, falso si no lo son).*/
+
+import java.util.Scanner;
+
+public class Ejercicio9_TP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int num1 = 0;
+		int num2 = 0;
+		boolean sonIguales = false;
+		String textoSonIguales = "";
+
+		System.out.println("Ingrese un numero");
+		num1 = input.nextInt();
+		System.out.println("Ingrese otro numero");
+		num2 = input.nextInt();
+
+		if (num1 == num2) {
+			sonIguales = true;
+			textoSonIguales = "son iguales";
+		} else {
+			sonIguales = false;
+			textoSonIguales = "son diferentes";
+
+		}
+
+		System.out.println("Los valores ingresados " + textoSonIguales);
+
+		input.close();
+	}
+
+}
